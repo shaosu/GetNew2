6c0f1cf05a4528277161c0a2f7294c61ecae2c81
ssl321 <1459945441@qq.com>
2016-05-03 23:05:43 +0800

    添加 文件Module.vb 包含namespace： SuApi Module：Module1

---
+++ ww1/Module1.vb
@@ -0,0 +1,11 @@
+﻿Namespace SuApi
+    Module Module1
+        Public Sub ShowInfo()
+            Console.WriteLine(SystemInformation.UserName)
+            Console.WriteLine(SystemInformation.ComputerName)
+
+        End Sub
+
+
+    End Module
+End Namespace
